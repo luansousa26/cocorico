6dad43ea74e87eff688bf765b9f34f3aea70152c
Luan Sousa <luan26@uninove.edu.br>
2018-08-12 14:12:20 -0300

    novas imagens adicionadas

--- src/app/layouts/personagens/personagens.component.ts
+++ src/app/layouts/personagens/personagens.component.ts
@@ -14,7 +14,17 @@ export class PersonagensComponent implements OnInit {
     this.personagens = [
       {nomePersonagem: 'Alípio ', descricaoPersonagem: 'Cavalo Alípio', imagemPersonagem: './assets/personagens/alipio2.jpg'},
       {nomePersonagem: 'Júlio ', descricaoPersonagem: 'Júlio', imagemPersonagem: './assets/personagens/julio2.jpg'},
-      {nomePersonagem: 'Caco ', descricaoPersonagem: 'Júlio', imagemPersonagem: './assets/personagens/caco.jpg'},
+      {nomePersonagem: 'Caco ', descricaoPersonagem: 'Caco', imagemPersonagem: './assets/personagens/caco.jpg'},
+      {nomePersonagem: 'Lilíca ', descricaoPersonagem: 'Lilíca', imagemPersonagem: './assets/personagens/lilica.jpg'},
+      {nomePersonagem: 'Zazá ', descricaoPersonagem: 'Zazá', imagemPersonagem: './assets/personagens/zaza.jpg'},
+      {nomePersonagem: 'Lola ', descricaoPersonagem: 'Lola', imagemPersonagem: './assets/personagens/lola.jpg'},
+      {nomePersonagem: 'Mimosa ', descricaoPersonagem: 'Vaca Mimosa', imagemPersonagem: './assets/personagens/mimosa.jpg'},
+      {nomePersonagem: 'Astolfo ', descricaoPersonagem: 'Porquinho Astolfo', imagemPersonagem: './assets/personagens/astolfo.jpg'},
+      {nomePersonagem: 'Toquinho ', descricaoPersonagem: 'Morcego Toquinho', imagemPersonagem: './assets/personagens/toquinho.jpg'},
+      {nomePersonagem: 'Oriba ', descricaoPersonagem: 'India Oriba', imagemPersonagem: './assets/personagens/oriba.jpg'},
+      {nomePersonagem: 'Dito e Feito ', descricaoPersonagem: 'Dito e Feito', imagemPersonagem: './assets/personagens/ditoefeito.jpg'},
+      {nomePersonagem: 'Avô e Avó ', descricaoPersonagem: 'Avô e Avó', imagemPersonagem: './assets/personagens/avoeavo.jpg'},
+      {nomePersonagem: 'João', descricaoPersonagem: 'João', imagemPersonagem: './assets/personagens/joao.jpg'},
     ]
   }
 
