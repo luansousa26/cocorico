f58fbb68c51add6ed4ef971e026c4645b6ad46e4
Luan Sousa <luan26@uninove.edu.br>
2018-08-12 13:09:55 -0300

    Model adicionada no component

--- src/app/layouts/carousel/carousel.component.ts
+++ src/app/layouts/carousel/carousel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CarouselModel } from './carousel.model';
 
 @Component({
   selector: 'app-carousel',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarouselComponent implements OnInit {
   
-  itensCarousel: any[] = [];
+  itensCarousel: CarouselModel[] = [];
   constructor() { }
 
   ngOnInit() {
