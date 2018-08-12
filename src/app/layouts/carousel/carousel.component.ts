ab741d170c7b66de49cd624f26668a47ff47fbdb
Luan Sousa <luan26@uninove.edu.br>
2018-08-12 15:30:01 -0300

    Alteração

--- src/app/layouts/carousel/carousel.component.ts
+++ src/app/layouts/carousel/carousel.component.ts
@@ -13,10 +13,14 @@ export class CarouselComponent implements OnInit {
 
   ngOnInit() {
     this.itensCarousel = [
-      {nomeItem: 'Imagem ', descricaoItem: 'Descrição', imagemItem: './assets/carousel/cocorico1.jpg'},
-      {nomeItem: 'Imagem ', descricaoItem: 'Descrição', imagemItem: './assets/carousel/cocorico2.jpg'},
-      {nomeItem: 'Imagem ', descricaoItem: 'Descrição', imagemItem: './assets/carousel/cocorico3.jpg'},
-      {nomeItem: 'Imagem ', descricaoItem: 'Descrição', imagemItem: './assets/carousel/cocorico4.jpg'}
+      {nomeItem: 'Cocoricó ', descricaoItem: 'Todos os dias ás 10:40 AM', 
+      imagemItem: './assets/carousel/cocorico1.jpg'},
+      {nomeItem: 'Venha conheçer nossos personagens ', descricaoItem: 'Descrição', 
+      imagemItem: './assets/carousel/cocorico2.jpg'},
+      {nomeItem: 'Cocoricó na Tv Cultura ', descricaoItem: 'Descrição', 
+      imagemItem: './assets/carousel/cocorico3.jpg'},
+      {nomeItem: 'Imagem ', descricaoItem: 'Descrição', 
+      imagemItem: './assets/carousel/cocorico4.jpg'}
     ]
   }
 
