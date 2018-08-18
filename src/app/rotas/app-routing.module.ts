0466be070fa9f87fd7ec34930d6eaf4e5e4b39cc
Luan Sousa <luan26@uninove.edu.br>
2018-08-18 18:12:32 -0300

    Esquema alterado para rotas

--- src/app/rotas/app-routing.module.ts
+++ src/app/rotas/app-routing.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from '../telas/home/home.component';
 const routes: Routes = [
+  { path: '', component: HomeComponent },
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
