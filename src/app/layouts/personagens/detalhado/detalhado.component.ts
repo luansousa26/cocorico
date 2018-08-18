aa7305e3f453c28e399e02acda22745f2f5e66ab
Luan Sousa <luan26@uninove.edu.br>
2018-08-17 21:23:37 -0300

    Alteração no detalhado

--- src/app/layouts/personagens/detalhado/detalhado.component.ts
+++ src/app/layouts/personagens/detalhado/detalhado.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '../../../../../node_modules/@angular/material';
 
 @Component({
   selector: 'app-detalhado',
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetalhadoComponent implements OnInit {
 
-  constructor() { }
+  personagem: Object;
+  constructor(private dialogRef: MatDialogRef<DetalhadoComponent>,
+              @Inject(MAT_DIALOG_DATA) data) {
+                this.personagem = data.dados;
+              }
 
   ngOnInit() {
   }
