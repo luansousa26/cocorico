e624fc1fabfc4945bbb546ec8e6cb31c34d53d89
Luan Sousa <luan26@uninove.edu.br>
2018-08-21 22:59:59 -0300

    Alteração na tela de música

--- src/app/telas/musicas/musicas.component.ts
+++ src/app/telas/musicas/musicas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-musicas',
@@ -8,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class MusicasComponent implements OnInit {
 
   panelOpenState: boolean;
-  constructor() { }
+  musicas: any;
+  constructor(private sanitazer: DomSanitizer) { }
 
   ngOnInit() {
+    this.musicas = [
+      {
+        // tslint:disable-next-line:max-line-length
+        id: 1, titulo: 'Já falei mais de mil vezes', descricao: 'Zazá está sempre mal-humorada e fica implicandocom todo mundo. Cada vez que alguém faz uma coisa errada, ela diz: "já falei mais de mil vezes que não é para fazer isso!". Lilica, que quer sempre brincar, começa a brincar com isso e Zazá leva a sério.',
+        youtube: 'IbnedIp7jPc', icone: './assets/personagens/zaza.jpg'
+      }
+    ];
   }
-
+  corrigirUrlYoutube(video) {
+    return this.sanitazer.bypassSecurityTrustResourceUrl(`https://www.youtube.com/embed/${video.youtube}?ecver=2`);
+  }
 }
