108d18b5b50ff449e5f172fc10398a42925cc683
Luan Sousa <luan26@uninove.edu.br>
2018-08-21 22:28:56 -0300

    novo componente criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { VideosComponent } from './telas/videos/videos.component';
 import { AppRoutingModule } from './rotas/app-routing.module';
 import { HomeComponent } from './telas/home/home.component';
 import { FormsModule } from '@angular/forms';
+import { MusicasComponent } from './telas/musicas/musicas.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ import { FormsModule } from '@angular/forms';
     NoticiasComponent,
     DetalhadoComponent,
     VideosComponent,
-    HomeComponent
+    HomeComponent,
+    MusicasComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/telas/musicas/musicas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-musicas',
+  templateUrl: './musicas.component.html',
+  styleUrls: ['./musicas.component.scss']
+})
+export class MusicasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
