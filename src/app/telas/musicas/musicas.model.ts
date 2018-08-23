ca6a1db508dde03ef2cd5ff1d0ff1451048b4ad2
Luan Sousa <luan26@uninove.edu.br>
2018-08-22 21:46:30 -0300

    Correção no safeSource

--- src/app/telas/musicas/musicas.model.ts
+++ src/app/telas/musicas/musicas.model.ts
@@ -1,9 +1,10 @@
+import { SafeResourceUrl } from '@angular/platform-browser';
 export class Musicas {
     id?: number;
     titulo?: String;
     descricao?: String;
     youtube?: String;
     icone?: String;
-    safeUrl?: String;
+    safeUrl?: SafeResourceUrl;
     musica?: String;
 }
